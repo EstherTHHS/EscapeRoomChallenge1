70daecac0b01adf8255bc3099bb8ed6ff9d7928f
DESKTOP-12N1QU1\DELL <thethtoohtoosan2172000@gmail.com>
2022-07-12 13:56:31 +0630

    adding game3 js section

--- script.js
+++ script.js
@@ -113,6 +113,7 @@ let changeFlexDir = document.getElementById("flexDirectn");
 let fJustCont = document.getElementById("flexJustCont");
 let fAlign = document.getElementById("flexAlignItems");
 let cubes = document.getElementsByClassName("cubes");
+let noti = document.getElementById("noti");
 const containerClass = document.getElementsByClassName("flexPuzzle-container");
 const flexDirectionsArray = ["row", "row-reverse", "column", "column-reverse"];
 const flexJustyContentArray = [
@@ -131,7 +132,7 @@ const flexAlignArray = [
     "baseline",
 ];
 
-//eventlistener
+//button eventlistener
 
 displayFlex.addEventListener("click", function() {
     if (containerClass[0].style.display == "block") {
@@ -154,6 +155,76 @@ displayFlex.addEventListener("click", function() {
     checkPuzzle();
 });
 
+let itemDirlength = flexDirectionsArray.length;
+changeFlexDir.addEventListener("click", function() {
+    // console.log(this.textContent);
+
+    if (i <= itemDirlength) {
+        this.textContent = flexDirectionsArray[i];
+        containerClass[0].style.flexDirection = flexDirectionsArray[i];
+        i++;
+        if (
+            document.querySelector(".flexPuzzle-container").style.flexDirection ===
+            "column-reverse"
+        ) {
+            document.querySelector(".flexPuzzle-container").style.top = "1100px";
+            document.querySelector(
+                "#flexboxPuzzleModal .modal-content"
+            ).style.height = "1300px";
+        }
+        if (
+            document.querySelector(".flexPuzzle-container").style.flexDirection ===
+            "column"
+        ) {
+            document.querySelector(
+                "#flexboxPuzzleModal .modal-content"
+            ).style.height = "1300px";
+        }
+    }
+    if (i > itemDirlength) {
+        document.querySelector(".flexPuzzle-container").style.top = "300px";
+        document.querySelector("#flexboxPuzzleModal .modal-content").style.height =
+            "745px";
+        containerClass[0].style.flexDirection = "row";
+        this.textContent = "Change Flex";
+        i = 0;
+    }
+    checkPuzzle();
+});
+
+let justifyItems = flexJustyContentArray.length;
+fJustCont.addEventListener("click", function() {
+    if (i <= justifyItems) {
+        this.textContent = flexJustyContentArray[i];
+        containerClass[0].style.justifyContent = flexJustyContentArray[i];
+        i++;
+    }
+    if (i > justifyItems) {
+        i = 0;
+        this.textContent = "ChangeFlexDirection";
+        containerClass[0].style.justifyContent = "flex-start";
+    }
+    checkPuzzle();
+});
+
+let alignItems = flexAlignArray.length;
+let midItem = document.getElementsByClassName("cubes");
+fAlign.addEventListener("click", function() {
+    if (i <= alignItems) {
+        this.textContent = flexAlignArray[i];
+        containerClass[0].style.alignItems = flexAlignArray[i];
+        i++;
+    }
+
+    if (i > alignItems) {
+        i = 0;
+        this.textContent = "Change Flex Direction";
+        containerClass[0].style.alignItems = "flex-start";
+        midItem[2].classList.remove("itemHeight");
+    }
+    checkPuzzle();
+});
+
 function checkPuzzle() {
     if (displayFlex.innerHTML === "Flex") {
         document.querySelectorAll(".top")[0].style.background = "#7b1346";
@@ -162,6 +233,7 @@ function checkPuzzle() {
         document.querySelectorAll(".back")[0].style.background = "#7b1346";
         document.querySelectorAll(".left")[0].style.background = "#7b1346";
         document.querySelectorAll(".right")[0].style.background = "#7b1346";
+        checkDirection();
     } else {
         document.querySelectorAll(".top")[0].style.background = "#371b58";
         document.querySelectorAll(".bottom")[0].style.background = "#371b58";
@@ -169,5 +241,85 @@ function checkPuzzle() {
         document.querySelectorAll(".back")[0].style.background = "#371b58";
         document.querySelectorAll(".left")[0].style.background = "#371b58";
         document.querySelectorAll(".right")[0].style.background = "#371b58";
+    }
+}
+
+function checkDirection() {
+    if (displayFlex.innerHTML === "Flex" && changeFlexDir.innerHTML === "row") {
+        document.querySelectorAll(".top")[4].style.background = "#7b1346";
+        document.querySelectorAll(".bottom")[4].style.background = "#7b1346";
+        document.querySelectorAll(".front")[4].style.background = "#7b1346";
+        document.querySelectorAll(".back")[4].style.background = "#7b1346";
+        document.querySelectorAll(".left")[4].style.background = "#7b1346";
+        document.querySelectorAll(".right")[4].style.background = "#7b1346";
+
+        checkJustifyContent();
+    } else {
+        document.querySelectorAll(".top")[4].style.background = "#371b58";
+        document.querySelectorAll(".bottom")[4].style.background = "#371b58";
+        document.querySelectorAll(".front")[4].style.background = "#371b58";
+        document.querySelectorAll(".back")[4].style.background = "#371b58";
+        document.querySelectorAll(".left")[4].style.background = "#371b58";
+        document.querySelectorAll(".right")[4].style.background = "#371b58";
+    }
+}
+
+function checkJustifyContent() {
+    if (
+        displayFlex.innerHTML === "Flex" &&
+        changeFlexDir.innerHTML === "row" &&
+        fJustCont.innerHTML === "center"
+    ) {
+        document.querySelectorAll(".top")[1].style.background = "#7b1346";
+        document.querySelectorAll(".bottom")[1].style.background = "#7b1346";
+        document.querySelectorAll(".front")[1].style.background = "#7b1346";
+        document.querySelectorAll(".back")[1].style.background = "#7b1346";
+        document.querySelectorAll(".left")[1].style.background = "#7b1346";
+        document.querySelectorAll(".right")[1].style.background = "#7b1346";
+        document.querySelectorAll(".top")[3].style.background = "#7b1346";
+        document.querySelectorAll(".bottom")[3].style.background = "#7b1346";
+        document.querySelectorAll(".front")[3].style.background = "#7b1346";
+        document.querySelectorAll(".back")[3].style.background = "#7b1346";
+        document.querySelectorAll(".left")[3].style.background = "#7b1346";
+        document.querySelectorAll(".right")[3].style.background = "#7b1346";
+        checkAllButtons();
+    } else {
+        document.querySelectorAll(".top")[1].style.background = "#371b58";
+        document.querySelectorAll(".bottom")[1].style.background = "#371b58";
+        document.querySelectorAll(".front")[1].style.background = "#371b58";
+        document.querySelectorAll(".back")[1].style.background = "#371b58";
+        document.querySelectorAll(".left")[1].style.background = "#371b58";
+        document.querySelectorAll(".right")[1].style.background = "#371b58";
+        document.querySelectorAll(".top")[3].style.background = "#371b58";
+        document.querySelectorAll(".bottom")[3].style.background = "#371b58";
+        document.querySelectorAll(".front")[3].style.background = "#371b58";
+        document.querySelectorAll(".back")[3].style.background = "#371b58";
+        document.querySelectorAll(".left")[3].style.background = "#371b58";
+        document.querySelectorAll(".right")[3].style.background = "#371b58";
+    }
+}
+
+function checkAllButtons() {
+    if (
+        displayFlex.innerHTML === "Flex" &&
+        changeFlexDir.innerHTML === "row" &&
+        fJustCont.innerHTML === "center" &&
+        fAlign.innerHTML === "center"
+    ) {
+        document.querySelectorAll(".top")[2].style.background = "#7b1346";
+        document.querySelectorAll(".bottom")[2].style.background = "#7b1346";
+        document.querySelectorAll(".front")[2].style.background = "#7b1346";
+        document.querySelectorAll(".back")[2].style.background = "#7b1346";
+        document.querySelectorAll(".left")[2].style.background = "#7b1346";
+        document.querySelectorAll(".right")[2].style.background = "#7b1346";
+        noti.innerHTML = "Well done! You've solved it!";
+        noti.style.color = "#7b1346";
+    } else {
+        document.querySelectorAll(".top")[2].style.background = "#371b58";
+        document.querySelectorAll(".bottom")[2].style.background = "#371b58";
+        document.querySelectorAll(".front")[2].style.background = "#371b58";
+        document.querySelectorAll(".back")[2].style.background = "#371b58";
+        document.querySelectorAll(".left")[2].style.background = "#371b58";
+        document.querySelectorAll(".right")[2].style.background = "#371b58";
     }
 }
